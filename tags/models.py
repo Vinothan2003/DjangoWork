abd56d94213742fdd7ceb6a9058d960f9a3b0768
Vinothan2003 <vinothannc4@gmail.com>
2024-06-03 12:15:21 +0530

    Add

---
+++ tags/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+# from store.models import Product
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=225)
+
+
+class TaggedItem(models.Model):
+    # what tag applied to what object
+
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type(product, video, image)
+    # ID
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE) --> depended on store
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # -->GENERIC RELATIONSHIP
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey()
