330f5953400caaeb21d91c6632851c5576b4e8d7
Vinothan2003 <vinothannc4@gmail.com>
2024-06-04 19:11:19 +0530

    orm : [ADD]

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
@@ -13,6 +14,24 @@ def say_hello(request):
 
     # query_set = Product.objects.filter(pk=0).first()  # get product object and return
 
-    exists = Product.objects.filter(pk=1).exists()  # return boolean value
+    # exists = Product.objects.filter(pk=1).exists()  # return boolean value
 
-    return render(request, 'hello.html', {'name': 'Vinothan NC'})
+    # first(), exists() --> return single product
+
+    # queryset = Product.objects.filter(unit_price__gte=20).first() --> filtering objects
+
+    # query_set = Product.objects.filter(unit_price__range=(20, 50)).order_by('unit_price')
+
+    # query_set = Product.objects.filter(title__icontains='coffee')  # --> search using string
+    # last_update__year=2021 --> year
+    # description__isnull= True --> checking for null
+
+    # -- complex lookups--
+    # query_set = Product.objects.filter(unit_price__lt=20, inventory__lt=20) --> 1 - and
+    # query_set = Product.objects.filter(inventory__gt=20).filter(unit_price__gt=20) --> 2 - and
+    query_set = Product.objects.filter(
+        Q(inventory__gt=20) | Q(unit_price__gt=20)).order_by("unit_price")  # 3 - or
+
+    # Q(inventory__gt=20) | ~Q(unit_price__gt=20) --> 4 - ~not
+
+    return render(request, 'hello.html', {'name': 'Vinothan NC', 'products': list(query_set)})
